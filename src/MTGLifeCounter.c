c4d9fde4342ce8f72cc5f0ef8c74c18318dabe45
unknown <rschrader@L16416.clt.lash.loc>
2013-05-07 12:16:35 -0400

    Added screenshot image

--- src/MTGLifeCounter.c
+++ src/MTGLifeCounter.c
@@ -5,7 +5,7 @@
 #define MY_UUID { 0xC2, 0x5A, 0x8D, 0x50, 0x10, 0x5F, 0x45, 0xBF, 0xC9, 0x92, 0xCE, 0xF9, 0x58, 0xAC, 0x93, 0xAD }
 PBL_APP_INFO(MY_UUID,
              "M:TG Life", "Rick Schrader",
-             1, 0, /* App version */
+             1, 1, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
