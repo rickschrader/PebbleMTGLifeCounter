50a9d66e44ea38abdcf02d5a11277bf01b072547
rickschrader <rickschrader@gmail.com>
2013-05-17 18:26:31 -0700

    1.5 - Reduce long press repeat to 500ms

--- src/MTGLifeCounter.c
+++ src/MTGLifeCounter.c
@@ -6,7 +6,7 @@
 #define MY_UUID { 0xC2, 0x5A, 0x8D, 0x50, 0x10, 0x5F, 0x45, 0xBF, 0xC9, 0x92, 0xCE, 0xF9, 0x58, 0xAC, 0x93, 0xAD }
 PBL_APP_INFO(MY_UUID,
              "M:TG Life", "Rick Schrader",
-             1, 4, /* App version */
+             1, 5, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
@@ -24,6 +24,7 @@ AppTimerHandle _longPressDownTimerHandle;
 //Lets us differentiate between multiple timers
 #define CookieLongPressUpTimer 1
 #define CookieLongPressDownTimer 2
+#define LongPressRepeatMilliseconds 500
 
 #define InitialCount 20
 #define MaxHours 9
@@ -98,7 +99,7 @@ void IncrementAndDisplayA()
 
 void IncrementAHandler(ClickRecognizerRef recognizer, Window *window) {
 	//set the first timer
-	_longPressUpTimerHandle = app_timer_send_event(app, 900 /* milliseconds */, CookieLongPressUpTimer);
+	_longPressUpTimerHandle = app_timer_send_event(app, LongPressRepeatMilliseconds, CookieLongPressUpTimer);
 	
 	IncrementAndDisplayA();
 }
@@ -117,7 +118,7 @@ void IncrementAndDisplayB()
 
 void IncrementBHandler(ClickRecognizerRef recognizer, Window *window) {
 	//set the first timer
-	_longPressDownTimerHandle = app_timer_send_event(app, 900 /* milliseconds */, CookieLongPressDownTimer);
+	_longPressDownTimerHandle = app_timer_send_event(app, LongPressRepeatMilliseconds, CookieLongPressDownTimer);
 		
 	IncrementAndDisplayB();
 }
@@ -156,13 +157,13 @@ void TimerHandler(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie)
 	{
 		//increment A & schedule next timer
 		IncrementAndDisplayA();
-		_longPressUpTimerHandle = app_timer_send_event(ctx, 700 /* milliseconds */, CookieLongPressUpTimer);
+		_longPressUpTimerHandle = app_timer_send_event(ctx, LongPressRepeatMilliseconds, CookieLongPressUpTimer);
 	}
 	else if (cookie == CookieLongPressDownTimer)
 	{
 		//increment B & schedule next timer
 		IncrementAndDisplayB();
-		_longPressDownTimerHandle = app_timer_send_event(ctx, 700 /* milliseconds */, CookieLongPressDownTimer);
+		_longPressDownTimerHandle = app_timer_send_event(ctx, LongPressRepeatMilliseconds, CookieLongPressDownTimer);
 	}
 	
 }
